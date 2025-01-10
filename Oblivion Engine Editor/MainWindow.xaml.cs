8f5c091b570ce85d60c7063a89f1181560dbcd5f
qzx <695239472@qq.com>
2025-01-10 21:43:04 +0800

    init project

---
+++ Oblivion Engine Editor/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Oblivion_Engine_Editor.GameProject;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oblivion_Engine_Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+            OpenProjectBrowserDialog();
+        }
+        private void OpenProjectBrowserDialog()
+        {
+            var projectBrowser = new BrowserDialog();
+            if(projectBrowser.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ## oblivion engine
 ## author: qzxnoct
 ## date: 2025.1.10
-### This is a game engine written in C++.
+### This is a game engine written in C++/C#.
 ### It is still in development.
 ### It is based on the course "Game Engine Programming" , There is the link of the basis course: [Game Engine Programming](https://www.youtube.com/watch?v=hRL56gXqj-4&list=PLU2nPsAdxKWQYxkmQ3TdbLsyc1l2j25XM)
 ### And there would be more documents or related books or videos in the future.
